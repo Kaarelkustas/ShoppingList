4eb229f9f646033be2ba3838dc0050e58ffc1f4e
Opilane TTHK <opilane@tthk.ee>
2020-11-20 09:51:50 +0200

    first commit

---
+++ Fridayworkwithfiles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fridayworkwithfiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
